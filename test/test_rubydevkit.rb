fede2287f1afd044fcdc0135a751799ce2271e34
Lars Kanis <lars@greiz-reinsdorf.de>
2017-01-22 11:33:17 +0100

    Add test for rubydevkit

---
+++ test/test_rubydevkit.rb
@@ -0,0 +1,22 @@
+require "minitest/autorun"
+require "tempfile"
+
+class TestRubydevkit < Minitest::Test
+  def test_rubydevkit_without_params
+    skip unless File.directory?("C:/msys64")
+
+    out = Tempfile.open(%w[test .cmd]) do |fd|
+      fd.write <<-EOT
+@call rubydevkit
+@echo PATH: %PATH%
+@echo MSYSTEM: %MSYSTEM%
+      EOT
+      fd.close
+      IO.popen(["cmd", "/c", fd.path]){|io| io.read }
+    end
+
+    mingw = RUBY_PLATFORM =~ /x64/ ? "MINGW64" : "MINGW32"
+    assert_match(/PATH: .*;C:\\msys64\\#{mingw}\\bin;C:\\msys64\\usr\\bin/i, out)
+    assert_match(/MSYSTEM: #{mingw}/i, out)
+  end
+end
