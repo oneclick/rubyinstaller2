5f10d7a022755980b79186406a44497086edd71e
Lars Kanis <lars@greiz-reinsdorf.de>
2017-01-27 20:17:50 +0100

    Exchange rubydevkit by ridk.
    Fixup to commit c5f5921e7

---
+++ resources/files/ridk.cmd
@@ -0,0 +1,32 @@
+@echo off
+
+if "x%~1" == "xenable" (
+  goto :setvars
+)
+if "x%~1" == "xdisable" (
+  goto :setvars
+)
+if "x%~1" == "xexec" (
+  goto :exec
+)
+
+"%~dp0ruby" -x "%~f0" %*
+@exit /b %ERRORLEVEL%
+
+:exec
+rem pass the command to a bash shell
+setlocal
+for /f "delims=" %%x in ('"%~dp0ruby" --disable-gems -x %~f0 %1') do set "%%x"
+shift
+shift
+%0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+@exit /b %ERRORLEVEL%
+
+:setvars
+@echo on
+@for /f "delims=" %%x in ('"%~dp0ruby" --disable-gems -x %~f0 %*') do set "%%x"
+@exit /b %ERRORLEVEL%
+
+#!/mingw64/bin/ruby
+require "ruby_installer"
+RubyInstaller::Ridk.run!(ARGV)
