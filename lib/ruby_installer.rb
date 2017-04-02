8bbf84e01858992dd73e471ed57c1f42e1e89427
Lars Kanis <lars@greiz-reinsdorf.de>
2017-04-02 21:02:59 +0200

    Mark RubyInstaller.add_dll_directory as deprecated.
    The reason to not remove it immediately is, that there are at least two
    gems out there that already use the function in released versions.
    These are pg.gem and fxruby.gem. They needs to be adjusted.

--- lib/ruby_installer.rb
+++ lib/ruby_installer.rb
@@ -1,8 +1,14 @@
 module RubyInstaller
   autoload :Runtime, 'ruby_installer/runtime'
 
-  # See RubyInstaller::Runtime.add_dll_directory
-  def self.add_dll_directory(path, &block)
-    Runtime.add_dll_directory(path, &block)
+  class << self
+    extend Gem::Deprecate
+
+    # See RubyInstaller::Runtime.add_dll_directory
+    def add_dll_directory(path, &block)
+      Runtime.add_dll_directory(path, &block)
+    end
+
+    deprecate :add_dll_directory, %q["require 'ruby_installer/runtime'; RubyInstaller::Runtime.add_dll_directory"], 2017, 8
   end
 end
