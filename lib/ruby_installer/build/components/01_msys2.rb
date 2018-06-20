oneclick/rubyinstaller2

commit 3113c085032cb8a2dd67e3ff32fc16ff07b1945b

Enhance RubyBundle so that it's usable
- Show MSYS2 as selectable component
- Tell the user when MSYS2 is deleted+reinstalled or reused
- Make classic RubyInstaller compatible with RubyBundle
- Properly initialize MSYS2 after install

lib/ruby_installer/build/components/01_msys2.rb
@@ -8,10 +8,12 @@ class Msys2 < Base
 
   def needed?
     begin
-      print "MSYS2 seems to be "
-      msys.msys_path
-      puts green("already installed")
-      false
+      if msys.with_msys_apps_enabled { run_verbose("sh", "-lc", "true") }
+        puts "MSYS2 seems to be " + green("already installed")
+        false
+      else
+        true
+      end
     rescue Msys2Installation::MsysNotFound
       puts red("unavailable")
       true
@@ -47,7 +49,7 @@ class Msys2 < Base
     end
 
     puts "Run the MSYS2 installer ..."
-    if run_verbose(temp_path)
+    if run_verbose(temp_path) && msys.with_msys_apps_enabled { run_verbose("sh", "-lc", "true") }
       puts green(" Success")
     else
       puts red(" Failed")

packages/rubybundle/recipes/sandbox/70-add-msys2-installation.rake
@@ -9,6 +9,10 @@ file msysdir => [msys2_task.devtools, File.dirname(msysdir)] do |t|
   # These files are re-created on the target computer.
   chdir t.name do
     rm init_files
+
+    # Do not include pacman gnupg directory. These files are unique to each installation
+    # and are created at the first call of `sh -l`.
+    rm_r "etc/pacman.d/gnupg"
   end
 end
 self.sandboxfiles << msysdir

recipes/installer-inno/50-generate-filelist.rake
@@ -3,11 +3,24 @@ directory File.dirname(filelist_iss)
 file filelist_iss => [__FILE__, ovl_expand_file(sandbox_task.sandboxfile_listfile), ovl_expand_file(sandbox_task.sandboxfile_arch_listfile), File.dirname(filelist_iss)] do
   puts "generate #{filelist_iss}"
   out = sandbox_task.sandboxfiles.map do |path|
-    if File.directory?(path)
-      "Source: ../../#{path}/*; DestDir: {app}/#{path.gsub(sandboxdir+"/", "")}; Flags: recursesubdirs createallsubdirs"
+    reltosandbox_path = path.gsub(sandboxdir+"/", "")
+
+    if package.respond_to?(:msysdir) && reltosandbox_path.start_with?(package.msysdir)
+      components = "msys2"
+      flags = "uninsneveruninstall"
     else
-      "Source: ../../#{path}; DestDir: {app}/#{File.dirname(path.gsub(sandboxdir+"/", ""))}"
+      components = "ruby"
     end
+    args = if File.directory?(path)
+      flags = "recursesubdirs createallsubdirs #{flags}"
+      source = "../../#{path}/*"
+      dest = "{app}/#{reltosandbox_path}"
+    else
+      source = "../../#{path}"
+      dest = "{app}/#{File.dirname(reltosandbox_path)}"
+    end
+
+    "Source: #{source}; DestDir: #{dest}; Flags: #{flags}; Components: #{components}"
   end.join("\n")
   File.write(filelist_iss, out)
 end

recipes/installer-inno/events.iss
@@ -24,6 +24,9 @@ begin
 
       UnInstallOldVersion();
 
+      if IsComponentSelected('msys2') then
+        DeleteRubyMsys2Directory();
+
     end else
       MsgBox('Looks like you''ve got on older, unsupported Windows version.' #13 +
              'Proceeding with a reduced feature set installation.',
@@ -42,6 +45,13 @@ procedure CurPageChanged(CurPageID: Integer);
 begin
   if CurPageID = wpSelectDir then
     WizardForm.NextButton.Caption := SetupMessage(msgButtonInstall);
+
+  // Disable MSYS2 component install, when it is already present in the install directory
+  if CurPageID = wpSelectComponents then
+  begin
+    EnableMsys2Component(not Msys2AlreadyInstalled());
+    ComplistClickCheck(TObject.Create);
+  end;
 end;
 
 procedure RegisterPreviousData(PreviousDataKey: Integer);
@@ -73,4 +83,17 @@ begin
         ModifyRubyopt(['-Eutf-8']);
     end;
   end;
+end;
+
+function ShouldSkipPage(PageID: Integer): Boolean;
+begin
+
+{* Skip components page if RubyInstaller (not RubyBundle) is running and no previous Ruby MSYS2 directory is present. *}
+#if Defined(RubyBundle) == 0
+  if (PageID = wpSelectComponents) and (not Msys2AlreadyInstalled()) then
+    Result := True
+  else
+#endif
+{* In all other cases present the components page, to show what is getting installed. *}
+    Result := False;
 end;

recipes/installer-inno/ri_gui.iss
@@ -12,6 +12,8 @@ const
 
 var
   PathChkBox, PathExtChkBox, Utf8ChkBox, DevkitChkBox: TCheckBox;
+  CompLabel: TLabel;
+  ComplistPrevClickCheck: TNotifyEvent;
 
 function IsAssociated(): Boolean;
 begin
@@ -85,6 +87,25 @@ begin
   ShellExec('open', ridkpath, 'install', '', SW_SHOWNORMAL, ewNoWait, ErrorCode);
 end;
 
+procedure ComplistClickCheck(Sender: TObject);
+begin
+  if Msys2AlreadyInstalled() then
+    if (WizardForm.ComponentsList.Items.Count > 1) and WizardForm.ComponentsList.Checked[1] then
+      CompLabel.Caption := 'ATTENTION: MSYS2 is already present in the Ruby install directory. It will be deleted now and then reinstalled. Additional installed pacman packages will be removed.'
+    else
+      CompLabel.Caption := 'MSYS2 is already present in the Ruby install directory and will not be deleted. However it can be updated per `ridk install` on the last page of the installer.'
+  else
+    CompLabel.Caption := '';
+
+  ComplistPrevClickCheck(Sender);
+end;
+
+procedure EnableMsys2Component(enable: Boolean);
+begin
+  if WizardForm.ComponentsList.Items.Count > 1 then
+    WizardForm.ComponentsList.Checked[1] := enable;
+end;
+
 procedure InitializeGUI;
 var
   ChkBoxCurrentY: Integer;
@@ -92,6 +113,25 @@ var
   HostPage: TNewNotebookPage;
   URLText, TmpLabel: TNewStaticText;
 begin
+
+  {* Add label to components list *}
+
+  CompLabel := TLabel.Create(WizardForm);
+  CompLabel.Parent := WizardForm.SelectComponentsPage;
+  CompLabel.Left := WizardForm.ComponentsList.Left;
+  CompLabel.Width := WizardForm.ComponentsList.Width;
+  CompLabel.Height := ScaleY(40);
+  CompLabel.Top :=
+    WizardForm.ComponentsList.Top + WizardForm.ComponentsList.Height - CompLabel.Height;
+  CompLabel.AutoSize := False;
+  CompLabel.WordWrap := True;
+
+  WizardForm.ComponentsList.Height :=
+    WizardForm.ComponentsList.Height - CompLabel.Height - ScaleY(8);
+
+  {* Bypass click event on ComponentsList *}
+  ComplistPrevClickCheck := WizardForm.ComponentsList.OnClickCheck;
+  WizardForm.ComponentsList.OnClickCheck := @ComplistClickCheck;
 
   {* Path, and file association task check boxes *}
 

recipes/installer-inno/rubyinstaller.iss.erb
@@ -8,13 +8,17 @@
 ; License: Modified BSD License
 
 <% require "etc" %>
+<% rubybundle = package.respond_to?(:msysdir) %>
+<% if rubybundle %>
+#define RubyBundle
+<% end %>
 
 #define RubyVersion <%=q package.rubyver %>
 #define PackageRelease <%=q package.pkgrel %>
 #define RubyBuildPlatform <%=q package.ruby_arch %>
 
 ; Build Installer details using above values
-#define InstallerName "Ruby " + <%=q package.packagever %> + "-" + <%=q package.arch %>
+#define InstallerName "Ruby " + <%=q package.packagever %> + "-" + <%=q package.arch %> + "<%= " with MSYS2" if rubybundle %>"
 #define InstallerPublisher "RubyInstaller Team"
 #define InstallerHomepage "http://rubyinstaller.org"
 
@@ -82,9 +86,9 @@ AddPath=Add Ruby executables to your PATH
 AddPathHint=Select to make this Ruby installation available from everywhere.%nThis may affect existing Ruby installations.
 AssociateExt=Associate .rb and .rbw files with this Ruby installation
 AssociateExtHint=Select to enable running your Ruby scripts by double clicking%nor simply typing the script name at your shell prompt. This may%naffect existing Ruby installations.
-DevkitInstall=Run 'ridk install' to install MSYS2 and development toolchain.
+DevkitInstall=Run 'ridk install' to setup MSYS2 and development toolchain.
 DevkitInstall2=MSYS2 is required to install gems with C extensions.
-DevkitInstallHint=The MSYS2 development tools consists of the GCC compiler suite and%nmany common UNIX tools required to build OpenSource software.
+DevkitInstallHint=The MSYS2 development tools consists of the GCC compiler suite and%nmany common UNIX tools required to build OpenSource software.%nIt is safe to run `ridk install` even if MSYS2 is already installed.
 DefaultUtf8=Use UTF-8 as default external encoding.
 DefaultUtf8Hint=This sets the environment variable 'RUBYOPT=-Eutf-8'.%nRubyInstaller-2.6 will probably set this as default.%nSee the wiki for more information.
 MouseoverHint=TIP: Mouse over the above options for more detailed information.
@@ -151,6 +155,15 @@ Name: {group}\{cm:DocumentationTitle}\{cm:APIReferenceTitle,{#RubyVersion}}; Fil
 Name: {group}\{cm:RubyGemsDocumentationServerTitle}; Filename: {app}\bin\gem.cmd; Parameters: server --launch; IconFilename: {app}\share\doc\ruby\html\images\ruby-doc.ico; Flags: runminimized
 Name: {group}\{cm:StartCmdPromptWithRubyTitle}; Filename: {sys}\cmd.exe; Parameters: /E:ON /K {app}\bin\setrbvars.cmd; WorkingDir: {%HOMEDRIVE}{%HOMEPATH}; IconFilename: {sys}\cmd.exe
 Name: {group}\{cm:UninstallProgram,{#InstallerName}}; Filename: {uninstallexe}
+
+[Components]
+Name: "ruby"; Description: "Ruby-<%= package.rubyver %> base files"; Types: custom; Flags: fixed
+
+Name: "msys2"; Description: "MSYS2 development toolchain <%= Time.now.strftime("%Y-%m-%d") %>"; Types: <%= "custom" if rubybundle %>; Flags: disablenouninstallwarning <%= "fixed" unless rubybundle %>
+
+[Types]
+Name: "custom"; Description: "Default installation"; Flags: iscustom
+
 
 [Code]
 <%= ovl_read_file "recipes/installer-inno/util.iss" %>

recipes/installer-inno/util.iss
@@ -256,3 +256,14 @@ begin
   end else
     Log('No previous ruby version found');
 end;
+
+function Msys2AlreadyInstalled(): Boolean;
+begin
+  Result := DirExists(ExpandConstant('{app}/msys32')) or DirExists(ExpandConstant('{app}/msys64'));
+end;
+
+procedure DeleteRubyMsys2Directory();
+begin
+  DelTree(ExpandConstant('{app}/msys32'), True, True, True);
+  DelTree(ExpandConstant('{app}/msys64'), True, True, True);
+end;

commit 971dbf3534665f22ee594ce5bdff3968bb216932

Fix MSYS2 detection in "ridk install"
It stopped without download when MSYS2 is not yet installed.

This is broken since commit 3113c0850 .

Fixes #114

lib/ruby_installer/build/components/01_msys2.rb
@@ -8,14 +8,14 @@ class Msys2 < Base
 
   def needed?
     begin
-      if msys.with_msys_apps_enabled { run_verbose("sh", "-lc", "true") }
+      if msys.with_msys_apps_enabled(if_no_msys: :raise) { run_verbose("sh", "-lc", "true") }
         puts "MSYS2 seems to be " + green("properly installed")
         false
       else
         true
       end
     rescue Msys2Installation::MsysNotFound
-      puts red("unavailable")
+      puts "MSYS2 seems to be " + red("unavailable")
       true
     end
   end