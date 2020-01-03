64d86a5c9443cffe8c028da32fd1e1f2cb2a91bb
Lars Kanis <kanis@comcard.de>
2020-01-03 11:24:37 +0100

    Try to convert .irb_history file when encoding was switched to UTF-8
    Otherwise running irb might fail to start due to invalid encoding error.

--- resources/files/irbrc_predefiner.rb
+++ resources/files/irbrc_predefiner.rb
@@ -3,10 +3,20 @@ require 'irb'
 irbrc_file = IRB.enum_for(:rc_file_generators).first.call(IRB::IRBRC_EXT)
 
 if irbrc_file && !File.exist?(irbrc_file)
-  File.write(irbrc_file, <<-EOT)
+  File.write irbrc_file, <<-EOT
 require 'irb/ext/save-history'
 require 'irb/completion'
 
 IRB.conf[:SAVE_HISTORY] = 200
   EOT
+end
+
+# Try to convert .irb_history from locale to default_external, if it isn't encoded properly.
+# This is mainly for transition from CP* encodings to UTF-8.
+history_file = IRB.rc_file("_history")
+if File.exist?(history_file) && !(hist=File.read(history_file)).valid_encoding?
+  hist = hist.encode(Encoding.default_external, Encoding.find("locale"))
+  if hist.valid_encoding?
+    File.write(history_file, hist)
+  end
 end
