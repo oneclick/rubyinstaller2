b894160a7248f75a54e4fdc8f73c801401bfc78c
Lars Kanis <lars@greiz-reinsdorf.de>
2020-01-03 22:13:03 +0100

    Fix compatibility of reline with ruby-2.4

--- resources/files/readline.rb
+++ resources/files/readline.rb
@@ -1,2 +1,17 @@
+if RUBY_VERSION < "2.5"
+  class String
+    def grapheme_clusters
+      chars
+    end
+  end
+
+  class Array
+    alias any_wo_arg? any?
+    def any?(obj=nil, &block)
+      obj ? any_wo_arg? { |o| o==obj } : any_wo_arg?(&block)
+    end
+  end
+end
+
 require 'reline' unless defined? Reline
 Readline = Reline
