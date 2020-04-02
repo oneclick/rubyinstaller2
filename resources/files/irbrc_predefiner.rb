9e93057dd7eb5fbd1563baff8053a049d33700c5
Lars Kanis <kanis@comcard.de>
2020-04-02 15:51:27 +0200

    Adjust irb-history re-encoding for latest Reline
    Reline has been changed to always use UTF-8 on Windows.
    So we can't depend on Encoding.default_external, but have to use UTF-8 explicit.

--- resources/files/irbrc_predefiner.rb
+++ resources/files/irbrc_predefiner.rb
@@ -10,11 +10,11 @@ IRB.conf[:SAVE_HISTORY] = 200
   EOT
 end
 
-# Try to convert .irb_history from locale to default_external, if it isn't encoded properly.
-# This is mainly for transition from CP* encodings to UTF-8.
+# Try to convert .irb_history from locale to UTF-8, if it isn't encoded properly.
+# This is for transition from CP* encodings of RbReadline to UTF-8 or Reline.
 history_file = IRB.rc_file("_history")
-if File.exist?(history_file) && !(hist=File.read(history_file)).valid_encoding?
-  hist = hist.encode(Encoding.default_external, Encoding.find("locale"))
+if File.exist?(history_file) && !(hist=File.read(history_file, encoding: 'utf-8')).valid_encoding?
+  hist = hist.encode('utf-8', Encoding.find("locale"))
   if hist.valid_encoding?
     File.write(history_file, hist)
   end
