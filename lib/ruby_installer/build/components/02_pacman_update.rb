071ec345f7ce04eb3e7b789e7eac4b49c67c0250
Lars Kanis <lars@greiz-reinsdorf.de>
2020-09-05 11:25:29 +0200

    Install msys2-keyring only when necessary
    This is to avoid possible downgrades in the future, if installed msys2-keyring
    is more recent that the one bundled with rubyinstaller.

--- lib/ruby_installer/build/components/02_pacman_update.rb
+++ lib/ruby_installer/build/components/02_pacman_update.rb
@@ -15,12 +15,19 @@ class PacmanUpdate < Base
     require "csv"
 
     msys.with_msys_apps_enabled do
-      puts "Update keyring according to https://www.msys2.org/news/#2020-06-29-new-packagers"
-      tar_path = download("http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz")
-      sig_path = download("http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig")
-      run_verbose("pacman-key", "--verify", sig_path)
-      tf = Tempfile.new
-      run_verbose("pacman", "-U", "--noconfirm", "--config", tf.path, tar_path)
+      puts "Check msys2-keyring version:"
+      IO.popen(%w[pacman -Q msys2-keyring>=r21], err: :out, &:read)
+      if $?.success?
+        puts green(" -> up-to-date")
+      else
+        puts yellow(" -> Update keyring according to https://www.msys2.org/news/#2020-06-29-new-packagers")
+
+        tar_path = download("http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz")
+        sig_path = download("http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig")
+        run_verbose("pacman-key", "--verify", sig_path)
+        tf = Tempfile.new
+        run_verbose("pacman", "-U", "--noconfirm", "--config", tf.path, tar_path)
+      end
 
       puts "Remove catgets to avoid conflicts while update  ..."
       # See https://github.com/Alexpux/MSYS2-packages/issues/1141
