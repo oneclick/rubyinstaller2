cc4c963ab33fdb22cc644457531d74b85eba9a2c
Lars Kanis <lars@greiz-reinsdorf.de>
2020-10-04 16:38:49 +0200

    Disable MSYS2 env vars before "ridk use"
    If a particular MSYS2 environment was activated (by ridk enable),
    it wasn't switched on "ridk use".
    Now the MSYS2 environment is disabled before switching and must be re-enabled
    after "ridk use" with the help of the new ruby version.

--- resources/files/ridk_use/ridk.cmd
+++ resources/files/ridk_use/ridk.cmd
@@ -2,6 +2,11 @@
 
 if "x%~1" == "xuse" (
   @echo on
+  if defined RI_DEVKIT (
+    echo Disable MSYS2 variables for path %RI_DEVKIT%
+    for /f "delims=" %%x in ('ruby --disable-gems -S -x ridk.cmd disable') do @set "%%x"
+  )
+
   @for /f "delims=" %%x in ('%~dp0../bin/ruby --disable-gems '%~dp0ridk_use.rb' %*') do @set "%%x"
   @exit /b %ERRORLEVEL%
 )
@@ -11,3 +16,7 @@ if "x%~1" == "xuse" (
 "%RPATH%\bin\ridk.cmd" %*
 @exit /b %ERRORLEVEL%
 #%/
+
+#!/mingw64/bin/ruby
+require "ruby_installer/runtime"
+RubyInstaller::Runtime::Ridk.run!(ARGV)
