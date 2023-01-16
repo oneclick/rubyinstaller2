8e47f078f3448d05fe2d0e9ff1d5ef247a61d758
Lars Kanis <lars@greiz-reinsdorf.de>
2023-01-16 08:26:32 +0100

    Allow `file` task to be called without block
    Taken from https://github.com/ged/ruby-pg/commit/a98f12f0c69379c94eedade94e26e4766b049a4a#diff-9f4d6fb4324d6b451f84c6dbd686be0bbedb163ba040c0e4ff75b5240fa4878d

--- lib/ruby_installer/build/utils.rb
+++ lib/ruby_installer/build/utils.rb
@@ -108,7 +108,7 @@ EOT
   def file(name, *args, &block)
     task_once(name, block) do
       super(name, *args) do |ta|
-        block.call(ta).tap do
+        block&.call(ta).tap do
           raise "file #{ta.name} is missing after task executed" unless File.exist?(ta.name)
         end
       end
