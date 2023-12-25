d62f16a454b9ea5826bc43edd3344210d27d5be1
Lars Kanis <lars@greiz-reinsdorf.de>
2023-12-25 20:51:22 +0100

    Add test for string encoding of the Win32::Registry
    This is temporary until there is a test suite to Win32::Registry .

---
+++ test/test_win32-registry.rb
@@ -0,0 +1,29 @@
+#frozen_string_literal: true
+require "minitest/autorun"
+require "win32/registry"
+
+class TestWin32Registry < Minitest::Test
+  private def backslachs(path)
+    path.gsub("/", "\\")
+  end
+
+  TEST_REGISTRY_KEY = "SOFTWARE/ruby-win32-registry-test/"
+
+  def test_win32_registry
+    keys = []
+    Win32::Registry::HKEY_CURRENT_USER.create(backslachs(TEST_REGISTRY_KEY)) do |reg|
+      reg.create("abc EUR")
+      reg.create("abc €")
+      reg.each_key do |subkey|
+        keys << subkey
+      end
+    end
+    
+    assert_equal [Encoding::UTF_8] * 2, keys.map(&:encoding)
+    assert_equal ["abc EUR", "abc €"], keys
+  ensure    
+    Win32::Registry::HKEY_CURRENT_USER.open(backslachs(File.dirname(TEST_REGISTRY_KEY))) do |reg|
+      reg.delete_key File.basename(TEST_REGISTRY_KEY), true
+    end
+  end
+end
