2d86b7c76938c2e91b97e02271278d10a57224b7
YO4 <ysno@ac.auone-net.jp>
2026-01-07 20:41:50 +0900

    more appropriate and smart load path
    Added a note regarding other files that are loaded.

--- bin/startmenu.cmd
+++ bin/startmenu.cmd
@@ -1,4 +1,9 @@
 @echo off
 
+REM This is sufficient to load console_ui.rb, but files located in
+REM lib/ruby_installer/build cannot be loaded; instead, they are used
+REM from the libraries installed with the ruby executable.
+REM ie, ruby_installer/runtime/colors.rb is loaded from ruby installation.
+
 set dir=%~dp0
-ruby -r "%dir:\=/%../lib/ruby_installer/runtime/console_ui" "%dir:\=/%../resources/files/startmenu.rb"
+ruby -I "%dir:\=/%../lib" "%dir:\=/%../resources/files/startmenu.rb"
